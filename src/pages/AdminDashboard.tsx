e1bf54b5990217fb24e09efe4abb6d03274205ad
Kgad <kalisagad05@gmail.com>
2026-04-02 11:56:50 +0200

    added notifications

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -21,6 +21,13 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
